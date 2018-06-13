7d889f0bde11f411bdbb8c1ab6acfe408b1375ea
jeoffrey@dissard.net <jeoffrey@dissard.net>
2018-06-13 16:06:01 +0200

    16H05

--- script.js
+++ script.js
@@ -1,51 +1,103 @@
-var player;
+if (localStorage.getItem("StLaQuestion") == null) {
+    var LaQuestion = 0;
+    localStorage.setItem("StLaQuestion", LaQuestion);
+} else if (localStorage.getItem("StLaQuestion") == 10) {
+    var LaQuestion = 0;
+    localStorage.setItem("StLaQuestion", LaQuestion);
+} else {
+    var LaQuestion = localStorage.getItem("StLaQuestion");
+}
+var courses;
+var Btn1 = document.getElementById('BTN1');
+var Btn2 = document.getElementById('BTN2');
+var Btn3 = document.getElementById('BTN3');
+var Btn4 = document.getElementById('BTN4');
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('video-placeholder', {
-        width: 600,
-        height: 400,
-        videoId: 'e395NwhfsU0',
-        startSeconds: 14,
-        endSeconds: 25,
-        playerVars: {
-            color: 'white',
-            autoplay: 1,
-            autohide: 1,
-            controls: 0,
-            rel: 0,
-            showinfo: 0,
-            iv_load_policy: 3,
-            start: 14,
-            end: 19,
-            end: 25
-        },
-        events: {
-            onReady: initialize,
-            onReady: setPause
+var p1 = document.createElement('p');
+p1.innerHTML = LaQuestion + " : <i class=\"material-icons icovert\">mood</i>";
+
+
+
+
+    var Questions = new XMLHttpRequest();
+    Questions.open('GET', 'http://localhost:80/Exos-Javascript-Quiz/question.json');
+    Questions.onreadystatechange = Truc;
+    Questions.send(null);
+
+    function Truc() {
+        if (Questions.readyState === 4) {
+            courses = JSON.parse(Questions.responseText);
+            Btn1.innerHTML = courses.Questions[LaQuestion].Reponse1;
+            Btn2.innerHTML = courses.Questions[LaQuestion].Reponse2;
+            Btn3.innerHTML = courses.Questions[LaQuestion].Reponse3;
+            Btn4.innerHTML = courses.Questions[LaQuestion].Reponse4;
+
+
         }
-    });
-}
-function setPause(){
-    setTimeout(Pause,7000);
-}
-function Pause() {
-    player.pauseVideo();
-}
-function initialize(){
+    }
 
-    // Update the controls on load
-    updateTimerDisplay();
-    updateProgressBar();
+    var player;
 
-    // Clear any old interval.
-    clearInterval(time_update_interval);
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('video-placeholder', {
+            width: 600,
+            height: 400,
+            videoId: courses.Questions[LaQuestion].Question,
+            playerVars: {
+                color: 'white',
+                autoplay: 1,
+                autohide: 1,
+                controls: 0,
+                rel: 0,
+                showinfo: 0,
+                iv_load_policy: 3,
+                start: courses.Questions[LaQuestion].Start,
+                end: courses.Questions[LaQuestion].End
+            },
+            events: {
+                onReady: initialize,
+                onReady: setPause
+            }
+        });
+    }
 
-    // Start interval to update elapsed time display and
-    // the elapsed part of the progress bar every second.
-    time_update_interval = setInterval(function () {
+    function setPause() {
+        setTimeout(Pause, courses.Questions[LaQuestion].Pause);
+    }
+
+    function Pause() {
+        player.pauseVideo();
+    }
+
+    function initialize() {
+
+        // Update the controls on load
         updateTimerDisplay();
         updateProgressBar();
-    }, 1000)
-}
 
+        // Clear any old interval.
+        clearInterval(time_update_interval);
 
+        // Start interval to update elapsed time display and
+        // the elapsed part of the progress bar every second.
+        time_update_interval = setInterval(function () {
+            updateTimerDisplay();
+            updateProgressBar();
+        }, 1000)
+    }
+
+    Btn4.addEventListener("click", (reponse1) => {
+        if (Btn4.id == courses.Questions[LaQuestion].Valid) {
+            ++LaQuestion;
+            localStorage.setItem("StLaQuestion", LaQuestion);
+            var p = document.getElementsByTagName('p')[LaQuestion - 1];
+            p.innerHTML = LaQuestion + " : <i class=\"material-icons icovert\">mood</i>";
+            document.location.reload(true);
+        } else {
+            ++LaQuestion;
+            localStorage.setItem("StLaQuestion", LaQuestion);
+            var p = document.getElementsByTagName('p')[LaQuestion - 1];
+            p.innerHTML = LaQuestion + " : <i class=\"material-icons icorouge\">mood_bad</i>";
+            document.location.reload(true);
+        }
+    });
---
+++ var.js
@@ -0,0 +1,1 @@
+var LaQuestion = 0;
